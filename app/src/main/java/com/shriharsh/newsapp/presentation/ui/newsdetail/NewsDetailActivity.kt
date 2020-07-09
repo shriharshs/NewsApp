8a842a201389e233aadc0fd449f936f8eff7a47f
shriharsh <shriharsh.saboji@mhealth.techshriharsh.saboji@halodoc.com>
2020-07-09 14:11:15 +0530

    Chores - 1. version of the app dependencies are read from main module 2. Initial structuring/organising of the app folder structure for MVVM

---
+++ app/src/main/java/com/shriharsh/newsapp/presentation/ui/newsdetail/NewsDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.shriharsh.newsapp.presentation.ui.newsdetail
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.shriharsh.newsapp.R
+
+class NewsDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news_detail)
+    }
+}
---
+++ app/src/main/java/com/shriharsh/newsapp/presentation/ui/newsfeed/NewsFeedActivity.kt
@@ -0,0 +1,12 @@
+package com.shriharsh.newsapp.presentation.ui.newsfeed
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.shriharsh.newsapp.R
+
+class NewsFeedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news_feed)
+    }
+}
